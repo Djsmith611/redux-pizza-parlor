8507cba6904ccddd7eda3445f827743f501a3019
lons <lonsnw@gmail.com>
2024-04-21 10:58:08 -0500

    Adding header component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Header from '../Header/Header.jsx'
 import './App.css';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
+        <Header />
       </header>
   
       <img src='images/pizza_photo.png' />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+
+    return (
+        <>
+        <h1 className='App-title'>Prime Pizza</h1>
+        </>
+    )
+};
+
+export default Header;
