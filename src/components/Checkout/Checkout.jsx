33d4f3e75932aba76bba9b3499ae808baacc657b
lons <lonsnw@gmail.com>
2024-04-23 12:20:17 -0500

    Further drafting of mapping reducers to the DOM

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -13,20 +13,19 @@ function Checkout() {
 
     return (
         <main>
+            <h3>Step 3: Checkout</h3>
         {customerInfo.map((info) => (
             <div className='order-summary'>
-                <h2>Step 3: Checkout</h2>
                     <div className='customer-info'>
                         <p>{info.customer_name}</p>
                         <p>{info.street_address}</p>
                         <p>{info.city}, {info.zip}</p>
                     </div>
                     <div className='order-type'>
-                        <h3>{info.type}</h3>
+                        <h4>{info.type}</h4>
                     </div>
             </div>
         ))}
-        {pizzaArray.map((order) => (
             <div className='order-table'>
                 <table>
                     <thead>
@@ -35,15 +34,17 @@ function Checkout() {
                             <td>Cost</td>
                         </tr>
                     </thead>
-                    {/* This will need to come from the what's in Redux from the pizza selections; not sure what we're calling these things? */}
-                    <tbody key={order.id}>
-                        <tr>
-                            <td></td>
-                        </tr>
+                    <tbody>
+                        {pizzaArray.map((pizza) => (
+                            <tr key={pizza.id}>
+                                <td>{pizza.type}</td>
+                                <td>{pizza.cost}</td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table> 
             </div>
-        ))}
+
             <h2>Total: {}</h2>
             <button onClick={handleClick}>Checkout</button>
         </main>
