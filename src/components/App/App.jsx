ad69ff66998c6a503d59d3f790cccce13ab10eb8
David Smith <djsmith611@me.com>
2024-04-27 08:53:31 -0700

    conditionally rendered pizza image, and fixed styling of customer info

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,41 +1,42 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from '../Header/Header.jsx';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import Header from "../Header/Header.jsx";
 import CustomerInfo from "../CustomerInfo/CustomerInfo.jsx";
-import Checkout from '../Checkout/Checkout.jsx';
-import './App.css';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Checkout from "../Checkout/Checkout.jsx";
+import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Admin from "../Admin/Admin.jsx";
 
 function App() {
-
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#639C68',
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#639C68",
+      },
+      secondary: {
+        main: "#f44336",
+      },
     },
-    secondary: {
-      main: '#f44336',
-    },
-  },
-});
+  });
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <Header theme={theme} ThemeProvider={ThemeProvider}/>
-      </header>
-      <img src="images/pizza_photo.png" />
-      <p>Pizza is great.</p>
+    <div className="App">
       <Router>
+        <header className="App-header">
+          <Header theme={theme} ThemeProvider={ThemeProvider} />
+        </header>
         <Routes>
           {/* PRESS CMD + "/" TO UNCOMMENT A SECTION, PLEASE CHANGE PATH AND ELEMENT AS NEEDED */}
-          
+
           {/* <Route exact path='/' element={<Landing />} /> */}
           {/* <Route exact path='/select' element={<SelectPizza />} /> */}
-          <Route path='/checkout' element={<Checkout />} />
-          <Route path='/info' element={<CustomerInfo />} />
-          <Route path='/admin' element={<Admin />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/info" element={<CustomerInfo />} />
+          <Route path="/admin" element={<Admin />} />
         </Routes>
       </Router>
     </div>
