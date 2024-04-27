14342835e410fd925c089c9fbc76ecde543dc14a
David Smith <djsmith611@me.com>
2024-04-27 11:07:13 -0700

    created homepage, remapped select, fixed info styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,16 +2,15 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  useLocation,
 } from "react-router-dom";
 import Header from "../Header/Header.jsx";
-import PizzaImage from "../Header/PizzaImage.jsx";
 import CustomerInfo from "../CustomerInfo/CustomerInfo.jsx";
 import Checkout from "../Checkout/Checkout.jsx";
 import "./App.css";
 import Select_Pizza from "../Select_Pizza/Select_Pizza.jsx";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Admin from "../Admin/Admin.jsx";
+import LandingPage from "../LandingPage/LandingPage.jsx";
 
 function App() {
   const theme = createTheme({
@@ -30,11 +29,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <Router>
           <Header />
-          <PizzaImage />
           <Routes>
-            <Route exact path="/" element={<Select_Pizza />} />
-            {/* <Route exact path='/' element={<Landing />} /> */}
-            {/* <Route exact path='/select' element={<SelectPizza />} /> */}
+            <Route exact path='/' element={<LandingPage />} />
+            <Route path="/select" element={<Select_Pizza />} />
             <Route path="/checkout" element={<Checkout />} />
             <Route path="/info" element={<CustomerInfo />} />
             <Route path="/admin" element={<Admin />} />
