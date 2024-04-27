03af291a63b5710e5fcf55e1446fe17670bac393
lons <lonsnw@gmail.com>
2024-04-27 14:16:41 -0500

    Made header clickable -- sends you back to main page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,16 @@
 import { AppBar, Typography } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate();
+
+
+  const handleClick = () => {
+      navigate('/')
+}
+
   return (
-    <AppBar position="static" sx={{ bgcolor: "primary.main" }}>
+    <AppBar position="static" sx={{ bgcolor: "primary.main" }} onClick={() => {handleClick()}}>
       <Typography variant="h4" component="h1" sx={{padding: 1}} >
         Prime Pizza
       </Typography>
